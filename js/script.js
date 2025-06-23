9916340055fa30632daac9839f026afca5e83f86
Maxime Mons <pro.maxime.mons@gmail.com>
2025-06-23 23:35:10 +0200

    [REFACTO]

--- js/script.js
+++ js/script.js
@@ -9,16 +9,19 @@ export function init() {
 	if(isHome == null) {
 		//Not home page but link is not corrrect
 		//We have to redirect home
+		console.log("null");
 		document.location.search = "";
 	} else if(isHome) {
 		//We're on home page
 		//We have to set labels to default
 		//We have to init langs options
+		console.log("true");
 		translateInitWebsite(currentLang);
 		initLangCombobox();
 	} else {
 		//We're not on home page
 		//We need to set the values on the roulette
+		console.log("false");
 		params = queryToData(new URLSearchParams(document.location.search).get("q"));
 	}
 }
